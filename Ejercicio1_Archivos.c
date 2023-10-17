e4cb5f5191c6c15e365f9c4d0f722f7099d395cd
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2023-10-17 11:38:31 -0300

    Punteros

--- Ejercicio1_Archivos.c
+++ Ejercicio1_Archivos.c
@@ -2,8 +2,7 @@
 
 const char *NOM_ARCHIVO = "archivoEjemplo.bin";
 
-void Escribir_Producto(FILE *filePointer, int *Id);
-
+//Declarar los arreglos de esta forma permite el no tener que poner struct Producto producto desp, sino unicamente Producto producto
 typedef struct
 {
     int ID;
@@ -11,51 +10,68 @@ typedef struct
     float Precio;
 }Producto;
 
+//Funciones
+void Escribir_Producto(Producto *producto,FILE *filePointer, int *Id);
+void Leer_Producto(Producto *producto,FILE *filePointer);
+
 
 int main(){
     FILE *filePointer;
     int Id=0;
 
     Producto producto;    
+    for (int i = 0; i < 2; i++)
+    {
+        Escribir_Producto(&producto,filePointer, &Id);
+    }
+    
+    Leer_Producto(&producto,filePointer);
+    
+    return 0;
+}
 
-    Escribir_Producto(filePointer, &Id);
-    printf("Carga Terminada\n");
 
+void Escribir_Producto(Producto *producto,FILE *filePointer, int *Id){
+    // Abre el archivo en modo escritura binaria
+    filePointer = fopen(NOM_ARCHIVO, "w+b");    
+    //Cargo Id
+    producto->ID = *Id;
+    //Cargo Nombre del producto
+    printf("Ingrese el nombre del producto:\n");
+    scanf("%s", producto->Nombre);
+    //Cargo Precio del producto
+    printf("Ingrese el precio del producto:\n");
+    scanf("%f", &producto->Precio);
+    //Escribo en el archivo lo que cargue
+    fwrite(&producto, sizeof(Producto), 1, filePointer);
+    //Cierro el archivo
+    fclose(filePointer);
+    //Incremento el Id para que tengan distinto cada nuevo producto
+    Id++;
+}
+
+
+void Leer_Producto(Producto *producto,FILE *filePointer){
     // Abre el archivo en modo lectura binaria
     filePointer = fopen(NOM_ARCHIVO, "rb");
-
-    if (filePointer == NULL) {
-        printf("No se pudo abrir el archivo.\n");
-        return 1;
-    }
 
     // Lee el archivo hasta que feof() devuelva verdadero
     while (!feof(filePointer)) {
         size_t itemsRead = fread(&producto, sizeof(Producto), 1, filePointer);
         if (itemsRead == 1) { // Verifica que se leyó un registro completo
-            printf("Id: %d\nNombre: %s\nPrecio: $%f\n ", producto.ID, producto.Nombre, producto.Precio);
+            printf("Id: %d\nNombre: %s\nPrecio: $%f\n ", producto->ID, producto->Nombre, producto->Precio);
         }
     }
+    //Cierro el archivo
     fclose(filePointer);
-    printf("Lectura Terminada\n");
-    return 0;
 }
 
 
-void Escribir_Producto(FILE *filePointer, int *Id){
-    Producto producto;
 
-    filePointer = fopen(NOM_ARCHIVO, "w+b");    
 
-    producto.ID = *Id;
-    printf("Ingrese el nombre del producto:\n");
-    scanf("%s", producto.Nombre);
-    printf("Ingrese el precio del producto:\n");
-    scanf("%f", &producto.Precio);
-
-    printf("\nPrecio:%f\n", producto.Precio);
-
-    fwrite(&producto, sizeof(Producto), 1, filePointer);
-    fclose(filePointer);
-    Id++;
-}
+/*QUEDA MANEJAR ESTA PARTE DE LOS ERRORES
+    if (filePointer == NULL) {
+            printf("No se pudo abrir el archivo.\n");
+            return 1;
+    }
+*/
